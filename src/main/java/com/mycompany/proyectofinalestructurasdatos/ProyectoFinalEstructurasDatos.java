3192f010020d4d15caad9d1451f920321a4025a8
Yalam <jesus.guzman233350@potros.itson.edu.mx>
2024-11-28 18:05:31 -0600

    Se agrega el proyecto

---
+++ src/main/java/com/mycompany/proyectofinalestructurasdatos/ProyectoFinalEstructurasDatos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectofinalestructurasdatos;
+
+/**
+ *
+ * @author Edgar Martinez
+ */
+public class ProyectoFinalEstructurasDatos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
